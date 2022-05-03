54e51e905567badc9eaeeaeefcdae73454750b1a
denis <permwed@gmail.com>
2022-05-03 16:13:11 +0500

    i know node js auth

--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,22 @@
 const Router = require("express");
 const router = new Router();
+const authMiddlewa = require("./middleware/authMiddlewa");
+const rolehMiddlewa = require("./middleware/roleMiddleware");
 const authController = require("./authController");
+const { check } = require("express-validator");
 
-router.post("/registration", authController.registration);
+router.post(
+  "/registration",
+  [
+    check("username", "Имя пользовтаеля не должно быть пустым").notEmpty(),
+    check("password", "Пороль больше 3, но меньше 8 символов").isLength({
+      min: 3,
+      max: 8,
+    }),
+  ],
+  authController.registration
+);
 router.post("/login", authController.login);
-router.get("/users", authController.getUsers);
+router.get("/users", rolehMiddlewa(["USER"]), authController.getUsers);
 
 module.exports = router;
